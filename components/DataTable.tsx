79efaaa58927aceb68e22f01756565af384d2a04
chris <yosoypro_117@hotmail.com>
2025-12-30 19:51:49 -0600

    feat(components): add DataTable component using shadcn table

---
+++ components/DataTable.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const DataTable = <T,>({
+  bodyCellClassName,
+  bodyRowClassName,
+  columns,
+  data,
+  headerCellClassName,
+  headerClassName,
+  headerRowClassName,
+  rowKey,
+  tableClassName,
+}: DataTableProps<T>) => {
+  return (
+    <Table className={cn("custom-scrollbar", tableClassName)}>
+      <TableHeader className={headerClassName}>
+        <TableRow className={cn("hover:bg-transparent!", headerRowClassName)}>
+          {columns.map((column, i) => (
+            <TableHead key={i} className={cn("bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5", headerCellClassName)}>{column.header}</TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((row, rowIndex) => (
+          <TableRow key={rowKey(row, rowIndex)} className={cn("overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative", bodyRowClassName)}>
+            {columns.map((column, columnIndex) => (
+              <TableCell key={columnIndex} className={cn("py-4 first:pl-5 last:pr-5", bodyCellClassName)}>
+                {column.cell(row, rowIndex)}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
+
+export default DataTable
