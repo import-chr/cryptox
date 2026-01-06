574f5f44a7ac30060bde6716cb53f02009f5abcc
chris <yosoypro_117@hotmail.com>
2026-01-06 13:19:56 -0600

    feat(api): add searchCoins function to get api data

--- lib/coingecko.actions.ts
+++ lib/coingecko.actions.ts
@@ -35,3 +35,47 @@ export async function fetchFromCoinGecko<T>(
 
   return response.json();
 }
+
+export async function searchCoins(query: string): Promise<SearchCoin[]> {
+  const q = query.trim();
+
+  if(!q) return [];
+
+  const searchRes: SearchCoinResponse = await fetchFromCoinGecko("/search", { query: query });
+
+  const baseItemCoin = searchRes.coins.map((coin) => ({
+    id: coin.id,
+    name: coin.name,
+    symbol: coin.symbol,
+    market_cap_rank: coin.market_cap_rank,
+    thumb: coin.thumb,
+    large: coin.large,
+    data: {
+      price: 0,
+      price_change_percentage_24h: 0,
+    }
+  }));
+
+  if(!baseItemCoin.length) return [];
+
+  const ids = baseItemCoin.slice(0, 10).map(coin => coin.id).join(",");
+
+  const coinsMarketsRes: CoinMarketData[] = await fetchFromCoinGecko("/coins/markets", {
+    vs_currency: "usd",
+    ids,
+  });
+
+  const coinsMarketsItemPrice = new Map(coinsMarketsRes.map(coin => [coin.id, coin]));
+
+  return baseItemCoin.map(coin => {
+    const prices = coinsMarketsItemPrice.get(coin.id);
+
+    return {
+      ...coin,
+      data: {
+        price: prices?.current_price ?? 0,
+        price_change_percentage_24h: prices?.price_change_24h ?? 0,
+      },
+    };
+  });
+}
